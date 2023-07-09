faaabe8a9ffa90f25d5bef4ec139adb492129033
unknown <emailpabs@gmail.com>
2023-07-08 22:23:16 -0500

    Added first prompt and alert

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ function writePassword() {
   passwordText.value = password;
 
 }
+function generatePassword() {
+  var charnum = window.prompt("How many characters? (minimum 8, maximum 128)");
+  if (charnum < 8 || charnum > 128) {
+    window.alert("Number must be 8 - 128 characters!");
+    generatePassword()
+  }
+  prompt("Do you want lowercase letters?")
+  if("yes")
 
+  return "Howdy!"
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// lowercase, uppercase, numeric, and/or special characters
+// a few if statements
