9db9464c6ff1c532d24696afd83b11c82c021247
unknown <emailpabs@gmail.com>
2023-07-13 19:41:57 -0500

    Completed code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# PassGen1
-Password Generator
+# License
+MIT license
+
+# Sources
+https://chat.openai.com/share/b83d410f-9e0c-477c-85ec-99e8e0b0e7a6
+Multiple TA's and tutors, as well as rewatching previous zoom sessions.
--- script.js
+++ script.js
@@ -15,13 +15,34 @@ function generatePassword() {
     window.alert("Number must be 8 - 128 characters!");
     generatePassword()
   }
-  prompt("Do you want lowercase letters?")
-  if("yes")
+var upperCasedCharacters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lowerCasedCharacters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var numericCharacters = ['0','1','2','3','4','5','6','7','8','9'];
+var specialCharacters = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.'];
+var hasUpperCasedCharacters = confirm('Click OK to confirm including uppercase characters.');
+var hasLowerCasedCharacters = confirm('Click OK to confirm including lowercase characters.')
+var hasSpecialCharacters = confirm('Click OK to confirm including special characters.');
+var hasNumericCharacters = confirm('Click OK to confirm including numeric characters.'); 
+var password = ""
+var posspass = []
+if (hasUpperCasedCharacters) {
+  posspass = posspass.concat(upperCasedCharacters)};
+if (hasLowerCasedCharacters) {
+  posspass = posspass.concat(lowerCasedCharacters)};
+if (hasSpecialCharacters) {
+  posspass = posspass.concat(specialCharacters)};
+if (hasNumericCharacters) {
+  posspass = posspass.concat(numericCharacters)};  
+if (hasUpperCasedCharacters === false && hasLowerCasedCharacters === false && hasSpecialCharacters === false && hasNumericCharacters === false) {
+  alert('At least one character type must be selected!');
+  return "Try again";
+  }
 
-  return "Howdy!"
+  for (let i = 0; i < charnum; i++) {
+    password += posspass[(Math.floor(Math.random() * posspass.length))];
+  }
+  
+return password
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// lowercase, uppercase, numeric, and/or special characters
-// a few if statements
